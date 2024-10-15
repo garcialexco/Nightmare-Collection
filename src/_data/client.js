3488666f40a8b423fa1c75e3a6877b8122eadf18
garcialexco <garcialexco@gmail.com>
2024-10-15 07:25:59 -0600

    general update

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,15 +1,5 @@
 module.exports = {
-    "name": "Kendu Inu",
-    "email": "KenduInu@proton.me",
-    "socials": {
-        "telegram": "https://t.me/KenduInu",
-        "x/twitter": "https://x.com/kenduinu",
-        "dextools": "https://www.dextools.io/app/en/ether/pair-explorer/0xd9f2a7471d1998c69de5cae6df5d3f070f01df9f?t=1720905204455",
-        "coingecko": "https://www.coingecko.com/en/coins/kendu-inu",
-        "coinmarketcap": "https://coinmarketcap.com/currencies/kendu-inu/",
-        "reddit": "https://www.reddit.com/r/KenduInu_Ecosystem/",
-        "youtube": "https://www.youtube.com/@KenduInuArmy",
-    },
+    "name": "Nightmare Collection",
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     "domain": "https://www.kenduinu.com"
 };
